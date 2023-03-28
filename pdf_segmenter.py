60165b0adc8f1053a9ea8f865b4ae0221cd050dd
Alex C-G <alexcg@outlook.com>
2023-03-28 11:22:50 +0200

    chore: bump copyright
    Signed-off-by: Alex C-G <alexcg@outlook.com>

--- pdf_segmenter.py
+++ pdf_segmenter.py
@@ -1,4 +1,4 @@
-__copyright__ = "Copyright (c) 2020-2021 Jina AI Limited. All rights reserved."
+__copyright__ = "Copyright (c) 2020-2023 Jina AI Limited. All rights reserved."
 __license__ = "Apache-2.0"
 
 import io
